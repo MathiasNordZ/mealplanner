0ace181278f03cb350419e19f0da69e85a3dfd3a
Mathias Nord <mathias.erik.nord@gmail.com>
2024-10-15 16:56:47 +0200

    Creating positive and negative test for setName.

--- src/test/java/edu/ntnu/idi/bidata/GroceryTest.java
+++ src/test/java/edu/ntnu/idi/bidata/GroceryTest.java
@@ -1,29 +1,51 @@
 package edu.ntnu.idi.bidata;
 
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroceryTest {
-  @org.junit.jupiter.api.BeforeAll
-  static void init() {
-    Grocery grocery;
-  }
 
-  @org.junit.jupiter.api.Test
+  @Test
   void setQuantityPositiveTest() {
-    grocery = new Grocery(3, "Beef", "kg", 750, 2024, 10, 25);
+    Grocery grocery = new Grocery(3, "Beef", "kg", 750, 2024, 10, 25);
 
     grocery.setQuantity(5);
 
     assertEquals(5, grocery.getQuantity());
   }
 
-  @org.junit.jupiter.api.Test
+  @Test
   void setQuantityNegativeTest() {
-    grocery = new Grocery(3, "Beef", "kg", 750, 2024, 10, 25);
+    Grocery grocery = new Grocery(3, "Beef", "kg", 750, 2024, 10, 25);
 
-    grocery.setQuantity(-5);
-
-    assertEquals(5, grocery.getQuantity());
+    assertThrows(IllegalArgumentException.class, () -> grocery.setQuantity(-5));
   }
 
+  @Test
+  void setNamePositiveTest() {
+    Grocery grocery = new Grocery(3, "Beef", "kg", 750, 2024, 10, 25);
+
+    grocery.setName("Chicken");
+
+    assertEquals("Chicken", grocery.getName());
+  }
+
+  @Test
+  void setNameNegativeTest() {
+    Grocery grocery = new Grocery(3, "Beef", "kg", 750, 2024, 10, 25);
+
+    assertThrows(IllegalArgumentException.class, () -> grocery.getName());
+  }
+
+  @Test
+  void setExpirationDate() {
+  }
+
+  @Test
+  void setUnitOfMeasurement() {
+  }
+
+  @Test
+  void setPrice() {
+  }
 }
