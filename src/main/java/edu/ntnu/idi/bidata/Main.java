2c87302361178cdbc6053cadfd2ed479b67c7ead
Mathias Nord <mathias.erik.nord@gmail.com>
2024-10-30 14:15:32 +0100

    Removed unused import

--- src/main/java/edu/ntnu/idi/bidata/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/Main.java
@@ -1,5 +1,7 @@
 package edu.ntnu.idi.bidata;
 
+import java.time.LocalDate;
+
 /**
  * The main starting point of your application. Let this class create the
  * instance of your main-class that starts your application.
