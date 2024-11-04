46297cd7ed05e4afb602f52f9b40fe8c6b11337a
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-04 13:32:02 +0100

    Playing around with the methods to see how they work.

--- src/main/java/edu/ntnu/idi/bidata/UserInterface.java
+++ src/main/java/edu/ntnu/idi/bidata/UserInterface.java
@@ -6,14 +6,17 @@ public class UserInterface {
     Grocery rice = new Grocery(2F, "Rice", "kilogram", 100, 2026, 11, 20);
     Grocery riceTwo = new Grocery(5F, "Rice", "kilogram", 300, 2026, 11, 20);
     Grocery apple = new Grocery(1F, "Apple", "kilogram", 20, 2024, 11, 20);
+    Grocery carrot = new Grocery(1F, "Carrot", "kilogram", 20, 2024, 11, 20);
     fridge = new FoodStorage();
 
     fridge.addGrocery(rice);
     fridge.addGrocery(riceTwo);
+    fridge.addGrocery(apple);
+    fridge.addGrocery(carrot);
   }
 
   public void start() {
 
-    System.out.println(fridge.searchGrocery("rice"));
+    System.out.println(fridge.getSortedList());
   }
 }
