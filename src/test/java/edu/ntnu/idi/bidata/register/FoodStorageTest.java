10c3c758333dec96c6ab144e6de61e2f82849a32
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-05 21:21:19 +0100

    Added documentation to the first methods.

--- src/test/java/edu/ntnu/idi/bidata/register/FoodStorageTest.java
+++ src/test/java/edu/ntnu/idi/bidata/register/FoodStorageTest.java
@@ -9,6 +9,15 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ *
+ * @author Mathias Erik Nord
+ * @since 05.11.2024
+ *
+ * This is the test class for the class FoodStorage. The test class is supposed to test certain methods of the FoodStorage class.
+ * The test class does follow the Arrange, Act and Assert philosophy, where an object of the class FoodStorage is created. It is acted upon, and then asserted.
+ *
+ */
 class FoodStorageTest {
   private FoodStorage foodStorage;
   private Grocery milk;
@@ -16,6 +25,10 @@ class FoodStorageTest {
   private Grocery cheese;
   private Grocery soda;
 
+  /**
+   * The method <code>setUp</code> will be run before each test.
+   * This is to set up an instance of FoodStorage properly before each test.
+   */
   @BeforeEach
   void setUp() {
     LocalDate today = LocalDate.now();
@@ -30,10 +43,16 @@ class FoodStorageTest {
     chicken = new Grocery(1.2f, "Chicken", KILOGRAM, 120, year, month, day);
     cheese = new Grocery(0.75f, "Cheese", KILOGRAM, 85, year, month, day);
     soda = new Grocery(0.33f, "Coca Cola", LITER, 18, year, month, day);
+
+    foodStorage.addGrocery(chicken);
   }
 
+  /**
+   * This is a positive test for the <code>addGrocery</code> method.
+   * Will set a valid input and should return name of the added grocery.
+   */
   @Test
-  void addGrocery() {
+  void addGroceryPositiveTest() {
     foodStorage.addGrocery(milk);
 
     List<Grocery> groceries = foodStorage.searchGrocery("Milk");
@@ -41,31 +60,13 @@ class FoodStorageTest {
     assertEquals("Milk", groceries.getFirst().getName());
   }
 
+  /**
+   * This is a negative test for the <code>addGrocery</code> method.
+   * @throws IllegalArgumentException should be thrown,
+   * because the grocery that will be added is set to null.
+   */
   @Test
-  void removeGrocery() {
-    foodStorage.removeGrocery(milk, 1);
-
-    List<Grocery> groceries = foodStorage.searchGrocery("Milk");
-    assertThrows();
-  }
-
-  @Test
-  void searchGrocery() {
-  }
-
-  @Test
-  void valueOfExpiredGroceries() {
-  }
-
-  @Test
-  void listOfExpiredGroceries() {
-  }
-
-  @Test
-  void valueOfAllGroceries() {
-  }
-
-  @Test
-  void getSortedList() {
+  void addGroceryNegativeTest() {
+    assertThrows(IllegalArgumentException.class, () -> foodStorage.addGrocery(null));
   }
 }
