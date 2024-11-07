b7e2378406da6a8ceac8615be86079ec8c4b4f9a
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-07 19:45:55 +0100

    refactored to using List instead of HashSet

--- src/main/java/edu/ntnu/idi/bidata/CookBook.java
+++ src/main/java/edu/ntnu/idi/bidata/CookBook.java
@@ -1,29 +1,31 @@
 package edu.ntnu.idi.bidata;
 
 import edu.ntnu.idi.bidata.register.FoodStorage;
-
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import edu.ntnu.idi.bidata.entity.Grocery;
+import edu.ntnu.idi.bidata.Recipe;
 
 /**
  * This is a cookbook class that contains a set of recipes.
  */
 public class CookBook {
-  private List<Recipe> recipes;
+  private final HashSet<Recipe> recipes = new HashSet<>();
 
   public CookBook() {
     // Currently empty
+  }
+
+  public Set<Recipe> getRecipes() {
+    return recipes;
   }
 
   public void addRecipe(Recipe recipe) {
     recipes.add(recipe);
   }
 
-  public List<Recipe> getRecipes() {
-    return recipes;
+  public void removeRecipe(Recipe recipe) {
+    recipes.remove(recipe);
   }
-
-  public List<Recipe> recipeSuggestion(FoodStorage foodStorage) {
-    return recipes;
-  }
-
 }
