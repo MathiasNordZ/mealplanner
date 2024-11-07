a256f0dad9dc4966e6afa7004083d09c5c4d3353
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-07 14:35:27 +0100

    Removed some objects.

--- src/main/java/edu/ntnu/idi/bidata/UserInterface.java
+++ src/main/java/edu/ntnu/idi/bidata/UserInterface.java
@@ -6,20 +6,21 @@ import edu.ntnu.idi.bidata.register.FoodStorage;
 public class UserInterface {
   private FoodStorage fridge;
   public void init() {
-    Grocery rice = new Grocery(2F, "Rice", "kilogram", 100, 2026, 11, 20);
-    Grocery riceTwo = new Grocery(5F, "Rice", "kilogram", 300, 2026, 11, 20);
-    Grocery apple = new Grocery(1F, "Apple", "kilogram", 20, 2024, 11, 20);
-    Grocery carrot = new Grocery(1F, "Carrot", "kilogram", 20, 2024, 11, 20);
+    Grocery milk = new Grocery(2F, "Milk", "liter", 15, 2024, 11, 20);
+    Grocery milkTwo = new Grocery(1.75F, "Milk", "liter", 25, 2024, 11, 20);
+    Grocery milkThree = new Grocery(1F, "Milk", "liter", 25, 2024, 11, 30);
+    Grocery milkFour = new Grocery(1.75F, "Milk", "liter", 25, 2024, 11, 30);
+
     fridge = new FoodStorage();
 
-    fridge.addGrocery(rice);
-    fridge.addGrocery(riceTwo);
-    fridge.addGrocery(apple);
-    fridge.addGrocery(carrot);
+    fridge.addGrocery(milk);
+    fridge.addGrocery(milkTwo);
+    fridge.addGrocery(milkThree);
+    fridge.addGrocery(milkFour);
   }
 
   public void start() {
 
-    System.out.println(fridge.getSortedList());
+    System.out.println(fridge.searchGrocery("milk"));
   }
 }
