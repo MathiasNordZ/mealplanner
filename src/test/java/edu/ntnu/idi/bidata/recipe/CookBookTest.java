33ab2b307d0105885e0d0b9b4e84a911faa7dac9
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-13 18:48:59 +0100

    Created new test classes.

---
+++ src/test/java/edu/ntnu/idi/bidata/recipe/CookBookTest.java
@@ -0,0 +1,24 @@
+package edu.ntnu.idi.bidata.recipe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CookBookTest {
+
+    @Test
+    void getAllRecipes() {
+    }
+
+    @Test
+    void getRecipe() {
+    }
+
+    @Test
+    void addRecipe() {
+    }
+
+    @Test
+    void removeRecipe() {
+    }
+}
---
+++ src/test/java/edu/ntnu/idi/bidata/recipe/RecipeTest.java
@@ -0,0 +1,56 @@
+package edu.ntnu.idi.bidata.recipe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeTest {
+
+    @Test
+    void getRecipeName() {
+    }
+
+    @Test
+    void setRecipeName() {
+    }
+
+    @Test
+    void getRecipeDescription() {
+    }
+
+    @Test
+    void setRecipeDescription() {
+    }
+
+    @Test
+    void getCookingInstructions() {
+    }
+
+    @Test
+    void setCookingInstructions() {
+    }
+
+    @Test
+    void getIngredients() {
+    }
+
+    @Test
+    void mapInputValidation() {
+    }
+
+    @Test
+    void setIngredients() {
+    }
+
+    @Test
+    void getAmountOfServings() {
+    }
+
+    @Test
+    void setAmountOfServings() {
+    }
+
+    @Test
+    void isPossibleToCook() {
+    }
+}
