39fc386fa6e91be44809a69973b21d6be254e5cd
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-13 14:41:59 +0100

    Added som dummy values to init method

--- src/main/java/edu/ntnu/idi/bidata/UserInterface.java
+++ src/main/java/edu/ntnu/idi/bidata/UserInterface.java
@@ -2,41 +2,75 @@ package edu.ntnu.idi.bidata;
 
 import edu.ntnu.idi.bidata.entity.Grocery;
 import edu.ntnu.idi.bidata.register.FoodStorage;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+/**
+ * This is the interface class, that will take all interaction with the user.
+ */
 public class UserInterface {
   private FoodStorage fridge;
   private CookBook cookBook;
-  private Recipe recipe;
-  private Recipe recipeTwo;
 
+  private List<Grocery> createGroceries() {
+    List<Grocery> groceries = new ArrayList<>();
+
+    groceries.add(new Grocery(1F, "Beef", "kilogram", 250F, 2024, 12, 10));
+    groceries.add(new Grocery(1.75F, "Potato", "kilogram", 53F, 2024, 12, 31));
+    groceries.add(new Grocery(0.2F, "Bernaise", "liter", 65F, 2024, 12, 20));
+    groceries.add(new Grocery(0.75F, "Salmon", "kilogram", 179F, 2024, 12, 12));
+    groceries.add(new Grocery(0.5F, "Pasta", "kilogram", 58F, 2025, 8, 10));
+    groceries.add(new Grocery(0.25F, "Spinach", "kilogram", 25F, 2024, 12, 10));
+    groceries.add(new Grocery(1F, "Milk", "liter", 35F, 2024, 12, 10));
+
+    return groceries;
+  }
+
+  private List<Recipe> createRecipes() {
+    List<Recipe> recipes = new ArrayList<>();
+
+    Map<String, Float> ingredientsOne = new HashMap<>();
+    ingredientsOne.put("Beef", 0.75F);
+    ingredientsOne.put("Potatoes", 0.5F);
+    ingredientsOne.put("Bernaise", 0.20F);
+    recipes.add(new Recipe("Beef & Potato with Bernaise", "This is a dinner with grilled beef, mashed potatoes and bernaise sauce.", "1. Cook beef\n" +
+            "2. Cook potatoes.\n" +
+            "3. Make bernaise.\n" +
+            "4. Mash potatoes.", ingredientsOne, 4));
+
+    Map<String, Float> ingredientsTwo = new HashMap<>();
+    ingredientsTwo.put("Salmon", 0.50F);
+    ingredientsTwo.put("Pasta", 0.5F);
+    ingredientsTwo.put("Spinach", 0.20F);
+    recipes.add(new Recipe("Salmon with pasta and spinach", "This is a dinner with salmon, pasta and spinach.", "1. Cook salmon\n" +
+            "2. Cook pasta.\n" +
+            "3. Wash spinach.\n"
+            , ingredientsTwo, 4));
+
+    return recipes;
+  }
+
+  /**
+   * This is the method that initializes the program.
+   */
   public void init() {
-    Grocery chicken = new Grocery(2F, "Chicken", "kilogram", 150, 2024, 11, 20);
-    Grocery rice = new Grocery(1F, "Rice", "kilogram", 20, 2026, 11, 20);
-
     fridge = new FoodStorage();
     cookBook = new CookBook();
 
-    fridge.addGrocery(chicken);
-    fridge.addGrocery(rice);
+    List<Grocery> groceries = createGroceries();
+    for (Grocery grocery : groceries) {
+      fridge.addGrocery(grocery);
+    }
 
-    Map<String, Float> ingredients = new HashMap<>();
-    ingredients.put("Chicken", 2.1F);
-    ingredients.put("Rice", 1F);
-
-    recipe = new Recipe("Chicken & Rice", "Description", "Instructions", ingredients, 4);
-    recipeTwo = new Recipe("Chicken & Rice", "Description", "Instructions", ingredients, 4);
-
-    cookBook.addRecipe(recipe);
-    cookBook.addRecipe(recipeTwo);
-
+    List<Recipe> recipes = createRecipes();
+    for (Recipe recipe : recipes) {
+      cookBook.addRecipe(recipe);
+    }
   }
 
+  /**
+   * This is the method that will start the program.
+   */
   public void start() {
-    System.out.println(recipe.isPossibleToCook(fridge));
+    System.out.println(cookBook.getAllRecipes());
   }
 }
