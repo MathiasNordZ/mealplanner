08302a1474c18525e4ac40489fe9798384bb5c46
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-15 17:25:08 +0100

    Fixed Checkstyle.

--- src/main/java/edu/ntnu/idi/bidata/application/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/application/Main.java
@@ -5,6 +5,11 @@ package edu.ntnu.idi.bidata.application;
  * instance of your main-class that starts your application.
  */
 public class Main {
+  /**
+   * Where the program is run from.
+   *
+   * @param args ...
+   */
   public static void main(String[] args) {
     UserInterface ui = new UserInterface();
 
