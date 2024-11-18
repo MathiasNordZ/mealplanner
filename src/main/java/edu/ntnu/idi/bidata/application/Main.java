d96669b3e34381d366d8be290c1954620a612043
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-18 21:27:35 +0100

    Creation of TUI menu.

--- src/main/java/edu/ntnu/idi/bidata/application/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/application/Main.java
@@ -12,8 +12,11 @@ public class Main {
    */
   public static void main(String[] args) {
     UserInterface ui = new UserInterface();
+    UserInterfaceMenu uiMenu = new UserInterfaceMenu();
 
-    ui.init();
-    ui.start();
+    uiMenu.mainMenu();
+
+    //ui.init();
+    //ui.start();
   }
 }
