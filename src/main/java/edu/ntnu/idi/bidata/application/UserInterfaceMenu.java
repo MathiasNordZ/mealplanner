8c25c56a829c9a0c1f20bf1c916df3882eec7f58
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-22 14:10:03 +0100

    Created cookbook menu.

--- src/main/java/edu/ntnu/idi/bidata/application/UserInterfaceMenu.java
+++ src/main/java/edu/ntnu/idi/bidata/application/UserInterfaceMenu.java
@@ -76,6 +76,26 @@ public class UserInterfaceMenu {
     }
   }
 
+  private enum CookBookCommand {
+    CREATE_RECIPE(1),
+    BACK(0);
+
+    private final int value;
+
+    CookBookCommand(int value) {
+      this.value = value;
+    }
+
+    private static CookBookCommand fromValue(int value) {
+      for (CookBookCommand command : CookBookCommand.values()) {
+        if (command.value == value) {
+          return command;
+        }
+      }
+      throw new IllegalArgumentException("The command value is invalid" + value);
+    }
+  }
+
   /**
    * This method will display the main menu and handle the input of the main menu.
    */
@@ -99,7 +119,7 @@ public class UserInterfaceMenu {
 
       switch (command) {
         case MainCommands.GROCERY_MENU -> groceryMenu();
-        //case MainCommands.COOKBOOK_MENU -> cookBookMenu();
+        case MainCommands.COOKBOOK_MENU -> cookBookMenu();
         default -> System.out.println("Invalid command.");
       }
 
@@ -140,6 +160,35 @@ public class UserInterfaceMenu {
         default -> System.out.println("Invalid command.");
       }
     } while (command != GroceryCommand.BACK);
+  }
+
+  private void cookBookMenu() {
+    CookBookCommand command = null;
+
+    do {
+      System.out.println("""
+      [1] - Create recipe.
+      [2] - Remove recipe.
+      [3] - Print recipes.
+      [4] - Print recipe.
+      [5] - Recipe recommendation.
+      [0] - Go back.
+      """);
+
+      int commandValue = uiInputHandler.intReader("Enter your command: ");
+
+      try {
+        command = CookBookCommand.fromValue(commandValue);
+      } catch (IllegalArgumentException e) {
+        System.out.println("Invalid command." + e.getMessage());
+        continue;
+      }
+
+      switch (command) {
+        case CookBookCommand.CREATE_RECIPE -> createGrocery();
+        default -> System.out.println("Invalid command.");
+      }
+    } while (command != CookBookCommand.BACK);
   }
 
   /**
