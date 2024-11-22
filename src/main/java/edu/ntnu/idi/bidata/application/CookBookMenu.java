cba957835eafbc837a390d255232ed486f7edb3a
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-22 20:49:33 +0100

    Creted a method so the user can create a recipe.

--- src/main/java/edu/ntnu/idi/bidata/application/CookBookMenu.java
+++ src/main/java/edu/ntnu/idi/bidata/application/CookBookMenu.java
@@ -1,11 +1,20 @@
 package edu.ntnu.idi.bidata.application;
 
+import edu.ntnu.idi.bidata.recipe.Recipe;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CookBookMenu {
   UserInputHandler uiInputHandler;
+  Recipe recipe;
 
   private enum CookBookCommand {
     CREATE_RECIPE(1),
     REMOVE_RECIPE(2),
+    PRINT_RECIPES(3),
+    PRINT_RECIPE(4),
+    RECIPE_RECOMMENDATION(5),
     BACK(0);
 
     private final int value;
@@ -47,9 +56,35 @@ public class CookBookMenu {
       }
 
       switch (command) {
-        //case CookBookCommand.CREATE_RECIPE -> createRecipe();
+        case CookBookCommand.CREATE_RECIPE -> createRecipe();
         default -> System.out.println("Invalid command.");
       }
     } while (command != CookBookCommand.BACK);
+  }
+
+  private void createRecipe() {
+    try {
+      String nameOfRecipe = uiInputHandler.stringReader("Please enter name of recipe: ");
+      String recipeDescription = uiInputHandler.stringReader("Please enter a recipe description: ");
+      String cookingInstructions = uiInputHandler.stringReader("Please enter cooking instructions: ");
+      int amountOfServings = uiInputHandler.intReader("Please enter amount of servings");
+
+      Map<String, Float> ingredients = new HashMap<>();
+      boolean isUserDone = false;
+
+      while (!isUserDone) {
+        String nameOfIngredient = uiInputHandler.stringReader("Please enter name of ingredient (type 'done' if finished): ");
+        if (nameOfIngredient.equalsIgnoreCase("Done")) {
+          isUserDone = true;
+        } else {
+          float quantityOfIngredient = uiInputHandler.intReader("Please enter required quantity of ingredient: ");
+          ingredients.put(nameOfIngredient, quantityOfIngredient);
+        }
+      }
+      recipe = new Recipe(nameOfRecipe, recipeDescription, cookingInstructions, ingredients, amountOfServings);
+      System.out.println(nameOfRecipe + " was created successfully!");
+    } catch (IllegalArgumentException e) {
+      System.out.println("An error occured: " + e.getMessage());
+    }
   }
 }
