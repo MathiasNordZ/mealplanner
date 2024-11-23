0aef978b3ae4dd71139de4e1d00de5d72e34299e
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-23 16:22:32 +0100

    created a string formatter for recipes.

--- src/main/java/edu/ntnu/idi/bidata/application/StringFormatter.java
+++ src/main/java/edu/ntnu/idi/bidata/application/StringFormatter.java
@@ -1,8 +1,11 @@
 package edu.ntnu.idi.bidata.application;
 
 import edu.ntnu.idi.bidata.entity.Grocery;
+import edu.ntnu.idi.bidata.recipe.CookBook;
+import edu.ntnu.idi.bidata.recipe.Recipe;
 import edu.ntnu.idi.bidata.register.FoodStorage;
 import java.util.List;
+import java.util.Set;
 
 /**
  * This class is a formatting class.
@@ -24,14 +27,35 @@ public class StringFormatter {
    */
   public static String formatGroceries(List<Grocery> groceries) {
     StringBuilder table = new StringBuilder();
-    String format = "%-20s %-10s %-10s %-15s %-15s\n";
+    String format = "| %-20s | %-10s | %-10s | %-15s | %-15s |\n";
     table.append(String.format(format, "Name", "Quantity", "Unit", "Price", "Expiry Date"));
-    table.append("----------------------------------------------------------------------\n");
+    table.append("--------------------------------------------------------------------------------------\n");
 
     for (Grocery grocery : groceries) {
       table.append(String.format(format, grocery.getName(),
           grocery.getQuantity(), grocery.getUnitOfMeasurement(),
           grocery.getPrice(), grocery.getExpirationDate()));
+    }
+    return table.toString();
+  }
+
+  /**
+   * This method is the template of how a recipe should look when printed.
+   *
+   * @param recipes The recipes to add to table.
+   * @return Will return a table according to the given format.
+   * @since 0.0.1
+   */
+  public static String formatRecipes(Set<Recipe> recipes) {
+    StringBuilder table = new StringBuilder();
+    String format = "| %-20s | %-50s | %-50s | %-30s | %-15s |\n";
+    table.append(String.format(format, "Name", "Description", "Instructions", "Ingredients", "Servings"));
+    table.append("----------------------------------------------------------------------------------------------------------------------------------------------------\n");
+
+    for (Recipe recipe : recipes) {
+      table.append(String.format(format, recipe.getRecipeName(),
+          recipe.getRecipeDescription(), recipe.getCookingInstructions(),
+          recipe.getIngredients(), recipe.getAmountOfServings()));
     }
     return table.toString();
   }
@@ -72,5 +96,9 @@ public class StringFormatter {
    */
   public static String formatGrocery(FoodStorage foodStorage, String groceryToSearch) {
     return formatGroceries(foodStorage.searchGrocery(groceryToSearch));
+  }
+
+  public static String formatRecipeList(CookBook cookBook) {
+    return formatRecipes(cookBook.getAllRecipes());
   }
 }
