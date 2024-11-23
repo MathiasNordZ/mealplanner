859ee324f55b58dcf166cf018d79b7d1b11b7dd4
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-23 15:06:15 +0100

    Refactored to one main formatting method, instead of one single formatter for every method,

--- src/main/java/edu/ntnu/idi/bidata/application/StringFormatter.java
+++ src/main/java/edu/ntnu/idi/bidata/application/StringFormatter.java
@@ -3,6 +3,8 @@ package edu.ntnu.idi.bidata.application;
 import edu.ntnu.idi.bidata.entity.Grocery;
 import edu.ntnu.idi.bidata.register.FoodStorage;
 
+import java.util.List;
+
 /**
  * @author Mathias Erik Nord
  * @version 0.0.1
@@ -11,36 +13,27 @@ public class StringFormatter {
   private StringFormatter() {
   }
 
-  public static String formatSortedGroceries(FoodStorage foodStorage) {
+  public static String formatGroceries(List<Grocery> groceries) {
     StringBuilder table = new StringBuilder();
     String format = "%-20s %-10s %-10s %-15s %-15s\n";
     table.append(String.format(format, "Name", "Quantity", "Unit", "Price", "Expiry Date"));
     table.append("----------------------------------------------------------------------\n");
-    for (Grocery grocery : foodStorage.getSortedList()) {
+
+    for (Grocery grocery : groceries) {
       table.append(String.format(format, grocery.getName(), grocery.getQuantity(), grocery.getUnitOfMeasurement(), grocery.getPrice(), grocery.getExpirationDate()));
     }
     return table.toString();
+  }
+
+  public static String formatSortedGroceries(FoodStorage foodStorage) {
+    return formatGroceries(foodStorage.getSortedList());
   }
 
   public static String formatExpiredGroceries(FoodStorage foodStorage, String providedExpiryDate) {
-    StringBuilder table = new StringBuilder();
-    String format = "%-20s %-10s %-10s %-15s %-15s\n";
-    table.append(String.format(format, "Name", "Quantity", "Unit", "Price", "Expiry Date"));
-    table.append("----------------------------------------------------------------------\n");
-    for (Grocery grocery : foodStorage.listOfExpiredGroceries(providedExpiryDate)) {
-      table.append(String.format(format, grocery.getName(), grocery.getQuantity(), grocery.getUnitOfMeasurement(), grocery.getPrice(), grocery.getExpirationDate()));
-    }
-    return table.toString();
+    return formatGroceries(foodStorage.listOfExpiredGroceries(providedExpiryDate));
   }
 
   public static String formatGrocery(FoodStorage foodStorage, String groceryToSearch) {
-    StringBuilder table = new StringBuilder();
-    String format = "%-20s %-10s %-10s %-15s %-15s\n";
-    table.append(String.format(format, "Name", "Quantity", "Unit", "Price", "Expiry Date"));
-    table.append("----------------------------------------------------------------------\n");
-    for (Grocery grocery : foodStorage.searchGrocery(groceryToSearch)) {
-      table.append(String.format(format, grocery.getName(), grocery.getQuantity(), grocery.getUnitOfMeasurement(), grocery.getPrice(), grocery.getExpirationDate()));
-    }
-    return table.toString();
+    return formatGroceries(foodStorage.searchGrocery(groceryToSearch));
   }
 }
