da4337384dfc0b53c75a3ed11d030a151dee6787
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-23 14:13:42 +0100

    created some dummy groceries and added to storage.

--- src/main/java/edu/ntnu/idi/bidata/application/UserInterface.java
+++ src/main/java/edu/ntnu/idi/bidata/application/UserInterface.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idi.bidata.application;
 
+import edu.ntnu.idi.bidata.entity.Grocery;
 import edu.ntnu.idi.bidata.menu.CookBookMenu;
 import edu.ntnu.idi.bidata.menu.GroceryMenu;
 import edu.ntnu.idi.bidata.menu.MainMenu;
@@ -21,6 +22,17 @@ public class UserInterface {
     GroceryMenu groceryMenu = new GroceryMenu(foodStorage, uiInputHandler);
     CookBook cookBook = new CookBook();
     CookBookMenu cookBookMenu = new CookBookMenu(uiInputHandler, cookBook, foodStorage);
+
+    Grocery milk = new Grocery(1f, "Milk", "liter", 25, "2024-12-31");
+    Grocery chicken = new Grocery(1.25f, "Chicken", "kilogram", 125, "2024-12-10");
+    Grocery rice = new Grocery(2f, "Rice", "kilogram", 45, "2025-10-30");
+    Grocery cola = new Grocery(1.5f, "Cola", "liter", 37, "2025-08-20");
+
+    foodStorage.addGrocery(milk);
+    foodStorage.addGrocery(chicken);
+    foodStorage.addGrocery(rice);
+    foodStorage.addGrocery(cola);
+
     mainMenu = new MainMenu(uiInputHandler, groceryMenu, cookBookMenu);
   }
 
