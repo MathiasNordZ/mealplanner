b0d07e29be1d68cd5b241507255fa7a665072051
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-23 15:15:43 +0100

    doc: Added documentation to all methods.

--- src/main/java/edu/ntnu/idi/bidata/application/StringFormatter.java
+++ src/main/java/edu/ntnu/idi/bidata/application/StringFormatter.java
@@ -13,6 +13,12 @@ public class StringFormatter {
   private StringFormatter() {
   }
 
+  /**
+   *
+   * @param groceries The groceries to add to table.
+   * @return Will return a table according to the given format.
+   * @since 0.0.1
+   */
   public static String formatGroceries(List<Grocery> groceries) {
     StringBuilder table = new StringBuilder();
     String format = "%-20s %-10s %-10s %-15s %-15s\n";
@@ -25,14 +31,34 @@ public class StringFormatter {
     return table.toString();
   }
 
+  /**
+   *
+   * @param foodStorage The foodStorage to check.
+   * @return Will a sorted list, formatted as a table
+   * @since 0.0.1
+   */
   public static String formatSortedGroceries(FoodStorage foodStorage) {
     return formatGroceries(foodStorage.getSortedList());
   }
 
+  /**
+   *
+   * @param foodStorage The foodStorage to check.
+   * @param providedExpiryDate Expiry date to sort by.
+   * @return Will return list of expired groceries, formatted as table.
+   * @since 0.0.1
+   */
   public static String formatExpiredGroceries(FoodStorage foodStorage, String providedExpiryDate) {
     return formatGroceries(foodStorage.listOfExpiredGroceries(providedExpiryDate));
   }
 
+  /**
+   *
+   * @param foodStorage The foodStorage to check.
+   * @param groceryToSearch The grocery to search for.
+   * @return Will return the grocery in a formatted table.
+   * @since 0.0.1
+   */
   public static String formatGrocery(FoodStorage foodStorage, String groceryToSearch) {
     return formatGroceries(foodStorage.searchGrocery(groceryToSearch));
   }
