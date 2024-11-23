3a015b520597ba159d165680e771296890a2dec9
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-23 15:09:58 +0100

    Removed toString method.

--- src/main/java/edu/ntnu/idi/bidata/recipe/Recipe.java
+++ src/main/java/edu/ntnu/idi/bidata/recipe/Recipe.java
@@ -213,26 +213,4 @@ public class Recipe {
     }
     return true;
   }
-
-  /**
-   * This is a formatting method, that will format the object to a string when printing the object.
-   * This is to make the object more readable, instead of getting an ID in return.
-   * The refactoring of this method was inspired by GitHub Copilot.
-   * Please refer to the report for more info.
-   *
-   * @return Will return a formatted string, with information about the object.
-   */
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("Name of recipe: ").append(getRecipeName()).append("\n")
-            .append("Description: ").append(getRecipeDescription()).append("\n")
-            .append("Cooking Instructions: ").append(getCookingInstructions()).append("\n")
-            .append("Amount of servings: ").append(getAmountOfServings()).append("\n")
-            .append("Ingredients: \n");
-    for (Map.Entry<String, Float> entry : ingredients.entrySet()) {
-      sb.append("  - ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
-    }
-    return sb.toString();
-  }
 }
