ab4e356cfbd89a62ba9358273a7fa36547c209e2
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-26 15:02:41 +0100

    Created test class for StringFormatter.

---
+++ src/test/java/edu/ntnu/idi/bidata/util/StringFormatterTest.java
@@ -0,0 +1,53 @@
+package edu.ntnu.idi.bidata.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringFormatterTest {
+
+
+  @Test
+  void formatGroceriesPositiveTest() {
+  }
+
+  @Test
+  void formatGroceriesNegativeTest() {
+  }
+
+  @Test
+  void formatRecipesPositiveTest() {
+  }
+
+  @Test
+  void formatRecipesNegativeTest() {
+  }
+
+  @Test
+  void formatSortedGroceriesPositiveTest() {
+  }
+
+  @Test
+  void formatSortedGroceriesNegativeTest() {
+  }
+
+  @Test
+  void formatExpiredGroceriesPositiveTest() {
+  }
+
+  @Test
+  void formatExpiredGroceriesNegativeTest() {
+  }
+
+  @Test
+  void formatGroceryPositiveTest() {
+  }
+
+  @Test
+  void formatGroceryNegativeTest() {
+  }
+
+  @Test
+  void formatRecipePositiveTest() {
+  }
+}
