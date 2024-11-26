58eb300cc8d410f85fe8498e869d1668716730b1
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-26 17:48:35 +0100

    created CookBookMenuTest

---
+++ src/test/java/edu/ntnu/idi/bidata/menu/CookBookMenuTest.java
@@ -0,0 +1,21 @@
+package edu.ntnu.idi.bidata.menu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CookBookMenuTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void cookBookMenuPositiveTest() {
+  }
+
+  @Test
+  void cookBookMenuNegativeTest() {
+  }
+}
