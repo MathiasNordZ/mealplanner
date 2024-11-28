645c4cbc00e32bc614a18aa5154896e9928f63ab
Mathias Nord <mathias.erik.nord@gmail.com>
2024-11-28 20:01:05 +0100

    Moved validation tests from RecipeTest to RecipeValidatorTest.

--- src/test/java/edu/ntnu/idi/bidata/recipe/RecipeTest.java
+++ src/test/java/edu/ntnu/idi/bidata/recipe/RecipeTest.java
@@ -142,30 +142,6 @@ class RecipeTest {
     }
 
     /**
-     * Positive test method for <code>mapInputValidation</code>.
-     * Will check that an exception is not thrown, when the provided parameter is valid.
-     */
-    @Test
-    void mapInputValidationPositiveTest() {
-        Map<String, Pair<Float, String>> validIngredients = new HashMap<>();
-        validIngredients.put("Tomato Sauce", Pair.create(0.25f, "kilogram"));
-
-        assertDoesNotThrow(() -> recipe.mapInputValidation(validIngredients));
-    }
-
-    /**
-     * Negative test method for <code>mapInputValidation</code>.
-     * Will check that an exception is thrown when the provided parameter is invalid.
-     */
-    @Test
-    void mapInputValidationNegativeTest() {
-        Map<String, Pair<Float, String>> invalidIngredients = new HashMap<>();
-        invalidIngredients.put(null, Pair.create(0.25f, "kilogram"));
-
-        assertThrows(IllegalArgumentException.class, () -> recipe.mapInputValidation(invalidIngredients));
-    }
-
-    /**
      * Positive test method for <code>setIngredients</code>.
      * Will check the ingredients are set correctly.
      */
